5e162520c95f61636b69327a5e1c8040278e38bf
Alikradi <50806236+Alikradi@users.noreply.github.com>
2019-08-21 14:44:19 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/****************************
+ * Hello.java
+ * **************************
+ * running java
+ * __________________________
+ * Ali Kradi
+ * 8/21/19
+ * # section 2
+ ***************************/
+
+
+public class Hello {
+ public static void printHeading() {
+  System.out.println("Ali Kradi");
+  System.out.println("Hello.java");
+  System.out.println("running java");
+  System.out.println("version date");
+  System.out.println("# section 2");
+  System.out.println();
+ }
+ 
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("hello");
+ }
+}
